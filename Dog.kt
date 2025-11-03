5ca510801e8b91f336d79bd16b32b4f1a29f9e0b
comfortabletriphazard <71904259+comfortabletriphazard@users.noreply.github.com>
2025-11-03 23:25:27 +0000

    Dog Data Class for SQLite

---
+++ Dog.kt
@@ -0,0 +1,11 @@
+package com.threetrees.yoscribbleswallet.database
+
+data class Dog(
+    val id: Int = 0,
+    val name: String,
+    val dateOfBirth: String,
+    val breed: String,
+    val color: String,
+    val sex: String,
+    val hometown: String
+)
